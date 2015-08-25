104489fadb9cf8631b8e4be81fa14a7b85b192ee
rrampr <rrampr@umich.edu>
2015-08-25 19:50:19 +0530

    Doing Problem 118 from Codeforces - Div II

--- 118_89_div2/p_118b.cpp
+++ 118_89_div2/p_118b.cpp
@@ -5,44 +5,44 @@
 using namespace std;
 
 string int_to_string(int k) {
-  std::ostringstream ss;
-    ss << k;
-    return ss.str();
+    std::ostringstream ss;
+        ss << k;
+        return ss.str();
 }
 
 int main() {
-  int n;
-  string output;
-  cin >> n;
-  for (int i = n; i >= 0; i--) {
-    for (int j = 0; j < 2 * i; ++j) {
-      output.push_back(' ');
+    int n;
+    string output;
+    cin >> n;
+    for (int i = n; i >= 0; i--) {
+        for (int j = 0; j < 2 * i; ++j) {
+            output.push_back(' ');
+        }
+        int mid = n - i;
+        for (int j = 0; j < mid; ++j) {
+            output.append(int_to_string(j) + " ");
+        }
+        for (int j = mid; j >= 0; --j) {
+            output.append(int_to_string(j) + " ");
+        }
+        output.pop_back();
+        output.push_back('\n');
     }
-    int mid = n - i;
-    for (int j = 0; j < mid; ++j) {
-      output.append(int_to_string(j) + " ");
-    }
-    for (int j = mid; j >= 0; --j) {
-      output.append(int_to_string(j) + " ");
-    }
-    output.pop_back();
-    output.push_back('\n');
-  }
 
-  for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < 2 * (i + 1); ++j) {
-      output.push_back(' ');
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < 2 * (i + 1); ++j) {
+            output.push_back(' ');
+        }
+        int mid = n - i - 1;
+        for (int j = 0; j < mid; ++j) {
+            output.append(int_to_string(j) + " ");
+        }
+        for (int j = mid; j >= 0; --j) {
+            output.append(int_to_string(j) + " ");
+        }
+        output.pop_back();
+        output.push_back('\n');
     }
-    int mid = n - i - 1;
-    for (int j = 0; j < mid; ++j) {
-      output.append(int_to_string(j) + " ");
-    }
-    for (int j = mid; j >= 0; --j) {
-      output.append(int_to_string(j) + " ");
-    }
-    output.pop_back();
-    output.push_back('\n');
-  }
-  cout << output;
-  return 0;
+    cout << output << "\n";
+    return 0;
 }
